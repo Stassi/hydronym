b24cba9d2f8f8c041880cd3e052d16163208e1e7
Andreas Stassivik <andreas@stassi.net>
2022-09-16 14:45:34 -0700

    remove initial test

--- packages/arc4/debugArithmetic.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from '@jest/globals'
-import { multiply } from './debugArithmetic'
-
-describe('terrible multiplication (debug)', () => {
-  it('should multiply 1 times 1 to equal 1', () => {
-    expect(multiply(1, 1)).toBe(2)
-  })
-})
