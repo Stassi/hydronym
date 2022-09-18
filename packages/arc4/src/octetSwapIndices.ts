ecc0851cd60d94c10fa59488ddb78445e5257482
Andreas Stassivik <andreas@stassi.net>
2022-09-17 22:16:52 -0700

    octetSwapIndices implementation

---
+++ packages/arc4/src/octetSwapIndices.ts
@@ -0,0 +1,17 @@
+import increment from './increment.js'
+
+export default function octetSwapIndices(
+  o: Uint8Array,
+  x: number,
+  y: number
+): Uint8Array {
+  const [lowIndex, highIndex] = [...[x, y]].sort()
+
+  return Uint8Array.from([
+    ...o.subarray(0, lowIndex),
+    o[highIndex],
+    ...o.subarray(increment(lowIndex), highIndex),
+    o[lowIndex],
+    ...o.subarray(increment(highIndex)),
+  ])
+}
