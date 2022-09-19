58249826a7ffbe248d69bf498db671e1735d2656
Andreas Stassivik <andreas@stassi.net>
2022-09-18 19:26:22 -0700

    schedule consolidation

---
+++ packages/arc4/src/schedule/schedule.ts
@@ -0,0 +1,22 @@
+import { indexedKey } from './indexedKey.js'
+import { sum } from '../arithmetic/addition.js'
+import swapIndices from '../octet/swapIndices.js'
+import {
+  divideByMaxOctet,
+  forEachMaxUIntOctet,
+  maxUIntOctet,
+} from '../octet/maximumOctet.js'
+
+export default function schedule(key: string): Uint8Array {
+  const atKeyIndex: (i: number) => number = indexedKey(key)
+
+  let j = 0,
+    s: Uint8Array = maxUIntOctet
+
+  forEachMaxUIntOctet((i: number) => {
+    j = divideByMaxOctet(sum(j, s[i], atKeyIndex(i)))
+    s = swapIndices(s, i, j)
+  })
+
+  return s
+}
