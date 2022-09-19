96ec40d3b67ed8121c06111579acd35bcded1432
Andreas Stassivik <andreas@stassi.net>
2022-09-18 18:36:53 -0700

    test simplification

--- packages/arc4/src/octet/swapIndices.test.ts
+++ packages/arc4/src/octet/swapIndices.test.ts
@@ -2,40 +2,40 @@ import { describe, expect, it } from '@jest/globals'
 import swapIndices from './swapIndices.js'
 
 describe('octet swap indices', () => {
-  describe('octet := [0, 40)', () => {
-    const octet = Uint8Array.from([0, 10, 20, 30])
+  describe('octets: [0, 4)', () => {
+    const octets = Uint8Array.from([0, 1, 2, 3])
 
     describe.each([
       {
         x: 0,
         y: 1,
-        expected: [10, 0, 20, 30],
+        expected: [1, 0, 2, 3],
       },
       {
         x: 1,
         y: 0,
-        expected: [10, 0, 20, 30],
+        expected: [1, 0, 2, 3],
       },
       {
         x: 1,
         y: 2,
-        expected: [0, 20, 10, 30],
+        expected: [0, 2, 1, 3],
       },
       {
         x: 2,
         y: 3,
-        expected: [0, 10, 30, 20],
+        expected: [0, 1, 3, 2],
       },
       {
         x: 0,
         y: 3,
-        expected: [30, 10, 20, 0],
+        expected: [3, 1, 2, 0],
       },
     ])(
-      'swap x: $x with y: $y',
+      'swap ( x: $x, y: $y )',
       ({ x, y, expected }: { x: number; y: number; expected: number[] }) => {
-        it(`should return ${expected}`, () => {
-          expect([...swapIndices(octet, x, y)]).toStrictEqual(expected)
+        it(`should return [${expected}]`, () => {
+          expect([...swapIndices(octets, x, y)]).toStrictEqual(expected)
         })
       }
     )
