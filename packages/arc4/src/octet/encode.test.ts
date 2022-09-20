3bdcf1bc512f4eb8082c094c425c75332dbdd4e4
Andreas Stassivik <andreas@stassi.net>
2022-09-20 16:21:15 -0700

    test latin-1 permutation encoding

--- packages/arc4/src/octet/encode.test.ts
+++ packages/arc4/src/octet/encode.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from '@jest/globals'
 import encode from './encode.js'
+import { maxUIntOctet } from './maximumOctet.js'
 
 describe('encode', () => {
   describe.each([
@@ -14,6 +15,11 @@ describe('encode', () => {
     {
       input: 'Secret',
       expected: [83, 101, 99, 114, 101, 116],
+    },
+    {
+      input:
+        '\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ',
+      expected: [...maxUIntOctet],
     },
   ])(
     'input: $input',
