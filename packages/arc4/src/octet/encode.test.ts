6b14948ab2c4fb588e1d15b21445332c22edca0d
Andreas Stassivik <andreas@stassi.net>
2022-09-20 15:21:30 -0700

    binary encoder test

---
+++ packages/arc4/src/octet/encode.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from '@jest/globals'
+import encode from './encode.js'
+
+describe('encode', () => {
+  describe.each([
+    {
+      input: 'Key',
+      expected: [75, 101, 121],
+    },
+    {
+      input: 'Wiki',
+      expected: [87, 105, 107, 105],
+    },
+    {
+      input: 'Secret',
+      expected: [83, 101, 99, 114, 101, 116],
+    },
+  ])(
+    'key: $key',
+    ({ input, expected }: { input: string; expected: number[] }) => {
+      it('should convert strings to octets', () => {
+        expect([...encode(input)]).toStrictEqual(expected)
+      })
+    }
+  )
+})
