8962b20ae52a61fbe0dc3f83621299b4dc0ab0ce
Andreas Stassivik <andreas@stassi.net>
2022-09-20 16:48:54 -0700

    standard naming convention ("encode" replaces "decode")

--- packages/arc4/src/schedule/indexedKey.ts
+++ packages/arc4/src/schedule/indexedKey.ts
@@ -1,12 +1,12 @@
-import encode from '../octet/encode.js'
 import { divideBy } from '../arithmetic/division.js'
+import { encode } from '../octet/transcode.js'
 import { length } from '../array/length.js'
 
 export default function indexedKey(key: string): (i: number) => number {
-  const decoded = encode(key),
-    divideByLength = divideBy(length(decoded))
+  const encoded = encode(key),
+    divideByLength = divideBy(length(encoded))
 
   return function atKeyIndex(i: number): number {
-    return decoded[divideByLength(i)]
+    return encoded[divideByLength(i)]
   }
 }
