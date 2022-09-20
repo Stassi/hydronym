ab2cc8e3ae4086db56827338ebec4898f00015c8
Andreas Stassivik <andreas@stassi.net>
2022-09-20 16:48:23 -0700

    binary, latin-1 transcoder consolidation

--- packages/arc4/src/octet/decode.ts
+++
@@ -1,3 +0,0 @@
-export default function decode(o: Uint8Array): string {
-  return Buffer.from(o).toString('latin1')
-}
--- packages/arc4/src/octet/encode.ts
+++
@@ -1,5 +0,0 @@
-import { Buffer } from 'node:buffer'
-
-export default function encode(s: string): Uint8Array {
-  return Uint8Array.from(Buffer.from(s, 'latin1'))
-}
