03513c440cce182243ec702150dc07f021284123
Andreas Stassivik <andreas@stassi.net>
2022-09-22 16:40:44 -0700

    state machine replaces side effects

--- packages/arc4/src/schedule/schedule.ts
+++ packages/arc4/src/schedule/schedule.ts
@@ -1,18 +1,12 @@
-import { forEachPermutation } from '../octet/maximum.js'
-import indexedKey from './indexedKey.js'
-import roundKey from './roundKey.js'
-import stream from './stream.js'
+import round from './round.js'
 import useState from '../state/useState.js'
 
 export default function schedule(key: string): Uint8Array {
-  const atKeyIndex = indexedKey(key),
-    { get: getRoundKey, set: setRoundKey } = useState(roundKey()),
-    { get: getStream, set: setStream } = useState(stream())
+  const { get: getRound, set: setRound } = useState(round(key))
 
-  forEachPermutation((i: number): void => {
-    setRoundKey(getRoundKey().addTo(atKeyIndex(i), getStream().atIndex(i)))
-    setStream(getStream().swapIndices(i, getRoundKey().state))
-  })
+  while (!getRound().isComplete()) {
+    setRound(getRound().next())
+  }
 
-  return getStream().state
+  return getRound().state.s
 }
