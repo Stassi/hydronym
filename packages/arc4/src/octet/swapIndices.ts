c26c819925b7955a598da8d5bd08a7936bf4a54f
Andreas Stassivik <andreas@stassi.net>
2022-10-01 03:07:29 -0700

    implement same-index swap

--- packages/arc4/src/octet/swapIndices.ts
+++ packages/arc4/src/octet/swapIndices.ts
@@ -1,11 +1,14 @@
 import { increment } from '../arithmetic/addition.js'
 import { ascending as sortAscending } from '../array/sort.js'
+import strictEquality from '../equality/strict.js'
 
 export default function swapIndices(
   o: Uint8Array,
   x: number,
   y: number
 ): Uint8Array {
+  if (strictEquality(x, y)) return o
+
   const [lowIndex, highIndex] = sortAscending([x, y])
 
   return Uint8Array.from([
