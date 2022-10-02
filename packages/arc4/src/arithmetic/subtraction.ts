3839dc03d0fb2a628c0c0296d20ff41f1998e587
Andreas Stassivik <andreas@stassi.net>
2022-10-02 01:03:42 -0700

    curried subtraction implementation

--- packages/arc4/src/arithmetic/subtraction.ts
+++ packages/arc4/src/arithmetic/subtraction.ts
@@ -1,3 +1,7 @@
 export function subtract(minuend: number, subtrahend: number): number {
   return minuend - subtrahend
 }
+
+export function subtractFrom(subtrahend: number) {
+  return (minuend: number): number => subtract(minuend, subtrahend)
+}
