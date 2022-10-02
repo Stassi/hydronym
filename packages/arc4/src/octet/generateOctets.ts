4680488871042af429a6c76334b19d41c008c097
Andreas Stassivik <andreas@stassi.net>
2022-10-02 15:59:19 -0700

    octet generation imperative implementation

---
+++ packages/arc4/src/octet/generateOctets.ts
@@ -0,0 +1,25 @@
+import { length } from '../array/length.js'
+import schedule from '../schedule/schedule.js'
+import swapIndices from './swapIndices.js'
+
+export default function generateOctets({
+  count,
+  key,
+}: {
+  count: number
+  key: string
+}): Uint8Array {
+  let i = 0
+  let j = 0
+  let s = schedule(key)
+  let k: Uint8Array = Uint8Array.from([])
+
+  while (count !== length(k)) {
+    i = (i + 1) % 256
+    j = (j + s[i]) % 256
+    s = swapIndices(s, i, j)
+    k = Uint8Array.from([...k, s[(s[i] + s[j]) % 256]])
+  }
+
+  return k
+}
