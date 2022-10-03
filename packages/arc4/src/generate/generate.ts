f0633156730af591690501ba811b56dadb188bba
Andreas Stassivik <andreas@stassi.net>
2022-10-03 15:23:54 -0700

    count underflow throws RangeError

--- packages/arc4/src/generate/generate.ts
+++ packages/arc4/src/generate/generate.ts
@@ -9,6 +9,8 @@ export default function generate({
   count: number
   key: string
 }): Uint8Array {
+  if (count < 1) throw new RangeError('Count must exceed 0')
+
   const { get: getRound, set: setRound } = useState(round({ count, key }))
 
   while (not(getRound().state.complete)) {
