ee88c9b039b1e7d6abc924acf78835825f731049
Andreas Stassivik <andreas@stassi.net>
2022-10-14 13:17:08 -0700

    curried strict equality implementation

--- packages/arc4/src/logic/strictEquality.ts
+++ packages/arc4/src/logic/strictEquality.ts
@@ -1,3 +1,7 @@
 export default function strictEquality(a: number, b: number) {
   return a === b
 }
+
+export function strictEquals(a: number) {
+  return (b: number): boolean => strictEquality(a, b)
+}
