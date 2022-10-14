ce43d36299bcd1cc0980beb87787c2502d58a091
Andreas Stassivik <andreas@stassi.net>
2022-10-14 13:19:20 -0700

    curried strict equality integration

--- packages/arc4/src/octet/maximum.ts
+++ packages/arc4/src/octet/maximum.ts
@@ -1,6 +1,6 @@
 import { divideBy } from '../arithmetic/division.js'
 import range from './range.js'
-import strictEquality from '../logic/strictEquality.js'
+import { strictEquals as strictEqualsCurried } from '../logic/strictEquality.js'
 
 const MAX_OCTET = 256
 
@@ -8,4 +8,5 @@ export const truncate: (dividend: number) => number = divideBy(MAX_OCTET)
 
 export const permutation: Uint8Array = range(MAX_OCTET)
 
-export const strictEquals = (n: number): boolean => strictEquality(MAX_OCTET, n)
+export const strictEquals: (n: number) => boolean =
+  strictEqualsCurried(MAX_OCTET)
