d21440e658e34af7a71a9d44dfa96d97657d4819
Andreas Stassivik <andreas@stassi.net>
2022-10-20 11:42:42 -0700

    output es module (no comments)

--- packages/binary-transcoder/webpack.config.mjs
+++ packages/binary-transcoder/webpack.config.mjs
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 import path, { dirname } from 'node:path'
 
 const PACKAGE_NAME = 'binaryTranscoder'
@@ -7,37 +8,27 @@ const PACKAGE_NAME = 'binaryTranscoder'
 export default {
   devtool: 'inline-source-map',
   entry: './src/index.ts',
+  experiments: { outputModule: true },
   mode: 'production',
   module: {
     rules: [
       {
         exclude: /node_modules/,
-        test: /\.([cm]?ts|tsx)$/,
-        use: [
-          {
-            // TODO: https://babeljs.io/docs/en/babel-preset-typescript
-            // TODO: https://www.npmjs.com/package/babel-loader
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { targets: 'defaults' }],
-                '@babel/preset-typescript'
-              ]
-            }
-          },
-          'ts-loader'
-        ]
+        test: /\.tsx?$/,
+        use: 'ts-loader'
       }
     ]
   },
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })]
+  },
   output: {
     filename: `${PACKAGE_NAME}.js`,
+    library: { type: 'module' },
     path: path.resolve(dirname(fileURLToPath(import.meta.url)), 'dist')
   },
   plugins: [
-    new NodePolyfillPlugin({
-      includeAliases: ['Buffer']
-    })
+    new NodePolyfillPlugin({ includeAliases: ['Buffer'] })
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
